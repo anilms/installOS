10cf88b482b9aed6464bd315fb80223f9aeb2407
Anil <anilms.mailbox@gmail.com>
2021-11-12 23:01:10 -0800

    [arch] Simplified gnome dconf load
        Put all gnome setting changes into one file

--- arch/install_3_DE_gnome_tweaks.sh
+++ arch/install_3_DE_gnome_tweaks.sh
@@ -12,44 +12,21 @@ sudo pacman -S \
     --noconfirm --needed \
 
 
-# Gnome sane settings
-# Refer here https://gist.github.com/grenade/6363978
-gsettings set org.gnome.desktop.wm.preferences audible-bell false
-gsettings set org.gnome.shell.app-switcher current-workspace-only true
-gsettings set org.gnome.software download-updates false
+# gnome conf
+dconf load / < gnome/my.dconf.txt
 
-gsettings set org.gnome.nautilus.list-view default-visible-columns "['name', 'size', 'type', 'date_modified']"
-gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
-gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+#git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+#git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
 
-#gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
-gsettings set org.gnome.desktop.interface show-battery-percentage true
-gsettings set org.gnome.desktop.interface toolbar-icons-size 'small'
+#cd WhiteSur-gtk-theme/
+#./install.sh -i arch -N glassy
+#cd -
 
-gsettings set org.gnome.desktop.interface font-name 'Noto Sans 10'
-gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 10'
-gsettings set org.gnome.desktop.interface monospace-font-name "Hack 9"
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Noto Sans Bold 10'
+#cd WhiteSur-icon-theme/
+#./install.sh
+#cd -
 
-# Keyboard shortcuts
-dconf load /org/gnome/desktop/wm/keybindings/ < gnome/wm-keybindings.dconf.bak
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < gnome/media-keys-keybindings.dconf.bak
-
-
-exit
-
-git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
-git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
-
-cd WhiteSur-gtk-theme/
-./install.sh -i arch -N glassy
-cd -
-
-cd WhiteSur-icon-theme/
-./install.sh
-cd -
-
-gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-light-solid'
-gsettings set org.gnome.shell.extensions.user-theme name 'WhiteSur-light'
-gsettings set org.gnome.desktop.interface icon-theme 'WhiteSur'
+#gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-dark-solid'
+#gsettings set org.gnome.shell.extensions.user-theme name 'WhiteSur-dark'
+#gsettings set org.gnome.desktop.interface icon-theme 'WhiteSur'
 
