8d93f8620f8ff6221d11cdf2bdd75eec3ee0517c
Anil <anilms.mailbox@gmail.com>
2021-11-12 19:39:03 -0800

    [arch] nonconfirm for aur installs

--- arch/install_5_aur.sh
+++ arch/install_5_aur.sh
@@ -3,8 +3,10 @@
 # https://wiki.archlinux.org/index.php/Arch_User_Repository
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si \
+    --noconfirm --needed
 
 yay -S \
     google-chrome \
-    visual-studio-code-bin
+    visual-studio-code-bin \
+    --noconfirm --needed
