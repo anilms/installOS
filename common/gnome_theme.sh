75e1de7cef0b21cdf412ded995bb85bb25a95722
Anil <anilms.mailbox@gmail.com>
2022-06-23 21:22:38 -0700

    [themes] Add WhiteSur wallpaper install

--- common/gnome_theme.sh
+++ common/gnome_theme.sh
@@ -10,6 +10,12 @@
 #./install.sh -i arch -N normal -HD --nord
 #cd -
 
+#git clone https://github.com/vinceliuice/WhiteSur-wallpapers.git
+#cd WhiteSur-wallpapers
+#sudo ./install-gnome-backgrounds.sh
+#install-wallpapers.sh
+#cd -
+
 #gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-dark-solid'
 #gsettings set org.gnome.shell.extensions.user-theme name 'WhiteSur-dark'
 #gsettings set org.gnome.desktop.interface icon-theme 'WhiteSur'
